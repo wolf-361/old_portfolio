4076bc7e70fb119facc25b9f12932fbf277a553a
Luc <wolf-361@hotmail.com>
2023-02-01 22:40:53 -0500

    main containers, debut icon/langage

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent {
+    public devSkills = [
+        { name: 'HTML5', iconpath: '../assets/icons/html.ico' },
+        { name: 'CSS3', iconpath: '../assets/icons/css3.ico' },
+        { name: 'JavaScript', iconpath: '../assets/icons/javascript.ico' },
+        { name: 'TypeScript', iconpath: '../assets/icons/typescript.ico' },
+        { name: 'Angular', iconpath: '../assets/icons/angular.ico' },
+        { name: 'Node.js', iconpath: '../assets/icons/nodejs.ico' },
+    ];
 
+        
 }
