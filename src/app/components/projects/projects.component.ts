07018e6de96fc3695cfbf3bf737bdcbf2493eb3d
Luc <wolf-361@hotmail.com>
2023-02-06 15:09:47 -0500

    Main frame of UI for projects, start of info

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
 })
 export class ProjectsComponent {
-
+  public projects = [
+    {
+      title: 'Auto hébergement',
+      description: 'Hébergement de ce site sur un serveur dédié chez moi',
+      list: [
+        'Achat d\'un serveur, configuration et exploitation chez moi', 
+        'Installation : Ubuntu server et Docker', 
+        'Sécurisation via Fail2Ban, firewall, monitoring, scan rootkit et cloudflare',
+        'Ce site est hébergé sur ce serveur',
+      ],
+      skills: [
+        {
+          title: 'Docker',
+          iconPath: '../assets/icons/docker.ico',
+        },
+      ],
+      link: null,
+      github: null,
+    },
+  ];
 }
