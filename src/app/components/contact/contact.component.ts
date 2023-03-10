d030b4480074de1e786e358a812ac68dc2e52ac7
wolf-361-D <68551568+wolf-361@users.noreply.github.com>
2023-03-10 12:21:47 -0500

    Basic framework for front of contact

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { env } from '../../../env';
+import axios, { Axios } from 'axios';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +10,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
+  public token: string|undefined;
+  public resolved: boolean = false;
+  public emailAdress: string = "Bien essayé :)";
 
+  constructor() {
+    this.token = undefined;
+  }
+
+  public send(form: NgForm): void {
+    if (form.invalid) {
+      for (const control of Object.keys(form.controls)) {
+        form.controls[control].markAsTouched();
+      }
+
+      return;
+    }
+
+    const data = {
+      response: this.token
+    };
+
+    axios.post(env.backendUrl, data).then((response) => {
+      if(response.data.success) {
+        this.emailAdress = response.data.email;
+        this.resolved = true;
+      
+      } else {
+        this.token = undefined;
+        alert("Capcha invalide");
+      } 
+
+    }).catch((error) => {
+
+      alert("Une erreur est survenue");
+    });
+  }
 }
