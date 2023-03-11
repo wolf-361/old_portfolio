98c37783000e88b337f6af7dd402f5b8b041c616
Luc <wolf-361@hotmail.com>
2023-03-11 16:05:49 -0500

    fix UI mobile/pc + update skills

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -7,20 +7,20 @@ import { Component } from '@angular/core';
 })
 export class SkillsComponent {
     public devSkills = [
+        { title: 'C', iconpath: '../assets/icons/c.ico' },
         { title: 'HTML5', iconpath: '../assets/icons/html.ico' },
         { title: 'CSS3', iconpath: '../assets/icons/css3.ico' },
         { title: 'JavaScript', iconpath: '../assets/icons/javascript.ico' },
         { title: 'TypeScript', iconpath: '../assets/icons/typescript.ico' },
         { title: 'Angular', iconpath: '../assets/icons/angular.ico' },
         { title: 'Java', iconpath: '../assets/icons/java.ico' },
-        { title: 'C', iconpath: '../assets/icons/c.ico' },
         { title: 'C++', iconpath: '../assets/icons/cpp.ico' },
+        { title: 'C#', iconpath: '../assets/icons/csharp.ico' },
         { title: 'Python', iconpath: '../assets/icons/python.ico' },
         { title: 'PHP', iconpath: '../assets/icons/php.ico' },
         { title: 'SQL', iconpath: '../assets/icons/sql.ico' },
         { title: 'Bootstrap', iconpath: '../assets/icons/bootstrap.ico' },
         { title: 'Material Design', iconpath: '../assets/icons/materialdesign.ico' },
-
     ];
 
     public networkSkills = [        
@@ -35,6 +35,12 @@ export class SkillsComponent {
         { title: 'Visual Studio', iconpath: '../assets/icons/vs.png' },
         { title: 'VS Code', iconpath: '../assets/icons/vscode.png' },
         { title: 'IntelliJ', iconpath: '../assets/icons/intellij.png' },
+        { title: 'CLion', iconpath: '../assets/icons/clion.png' },
+        { title: 'Rider', iconpath: '../assets/icons/rider.png' },
+        { title: 'Jira', iconpath: '../assets/icons/jira.ico' },
+        { title: 'Confluence', iconpath: '../assets/icons/confluence.ico' },
+        { title: 'Bitbucket', iconpath: '../assets/icons/bitbucket.ico' },
+        { title: 'PenPot', iconpath: '../assets/icons/penpot.ico' },
     ];
 
     public langagesSkills = [
