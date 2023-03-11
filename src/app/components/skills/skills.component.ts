52d7d46b3b24ec75a3b955a449df3e2715d5a875
Luc <wolf-361@hotmail.com>
2023-03-11 17:30:26 -0500

    ajout des icons manquant

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -17,10 +17,10 @@ export class SkillsComponent {
         { title: 'C++', iconpath: '../assets/icons/cpp.ico' },
         { title: 'C#', iconpath: '../assets/icons/csharp.ico' },
         { title: 'Python', iconpath: '../assets/icons/python.ico' },
-        { title: 'PHP', iconpath: '../assets/icons/php.ico' },
-        { title: 'SQL', iconpath: '../assets/icons/sql.ico' },
-        { title: 'Bootstrap', iconpath: '../assets/icons/bootstrap.ico' },
-        { title: 'Material Design', iconpath: '../assets/icons/materialdesign.ico' },
+        { title: 'PHP', iconpath: '../assets/icons/php.png' },
+        { title: 'SQL', iconpath: '../assets/icons/sql.png' },
+        { title: 'Bootstrap', iconpath: '../assets/icons/bootstrap.png' },
+        { title: 'Material Design', iconpath: '../assets/icons/materialdesign.png' },
     ];
 
     public networkSkills = [        
@@ -37,10 +37,10 @@ export class SkillsComponent {
         { title: 'IntelliJ', iconpath: '../assets/icons/intellij.png' },
         { title: 'CLion', iconpath: '../assets/icons/clion.png' },
         { title: 'Rider', iconpath: '../assets/icons/rider.png' },
-        { title: 'Jira', iconpath: '../assets/icons/jira.ico' },
-        { title: 'Confluence', iconpath: '../assets/icons/confluence.ico' },
-        { title: 'Bitbucket', iconpath: '../assets/icons/bitbucket.ico' },
-        { title: 'PenPot', iconpath: '../assets/icons/penpot.ico' },
+        { title: 'Jira', iconpath: '../assets/icons/jira.png' },
+        { title: 'Confluence', iconpath: '../assets/icons/confluence.png' },
+        { title: 'Bitbucket', iconpath: '../assets/icons/bitbucket.png' },
+        { title: 'PenPot', iconpath: '../assets/icons/penpot.png' },
     ];
 
     public langagesSkills = [
