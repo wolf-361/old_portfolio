2a04306edb1ccc3b06bb4102fadf7713836bb50b
Luc <wolf-361@hotmail.com>
2023-03-11 13:29:01 -0500

    optimise mobile for experience

--- src/app/components/experiences/experiences.component.ts
+++ src/app/components/experiences/experiences.component.ts
@@ -25,9 +25,9 @@ export class ExperiencesComponent {
       title: 'Vice-président des affaires académiques - AMI UQTR',
       date: '2022 - 2023',
       description:
-        "J'ai été élu vice-président des affaires académiques pour les session d'automne et d'hivers." +
+        "J'ai été élu vice-président des affaires académiques pour les session d'automne et d'hivers. " +
         "J'ai été responsable de supporter la formation académique en proposant des activités éducatives. " +
-        "De plus j'ai assuré la représentation de l'AMI sur les comités de programmes du DMI (Département de mathématique et d'informatique).",
+        "De plus j'ai assuré la représentation de l'AMI sur les comités de programmes du DMI (Département de mathématique et d'informatique). ",
       list: [
         'Création du site web et self hosting avec Wordpress (en cours)', 
         'Création d\'un artwork pour l\'association',
@@ -43,7 +43,7 @@ export class ExperiencesComponent {
       date: '2022',
       description:
         'Participation aux réunions hebdomadaires et aux conférences du groupe. ' +
-        'Mes projets principaux étant le site web du groupe et le retravailles des notes de cours.',
+        'Mes projets principaux étant le site web du groupe et le retravailles des notes de cours. ',
       list: ['Création du site web avec Wordpress (en cours)', 'Traduction et optimisation des expériences du cours CPH1020'],
       iconTitle: '',
       iconPath: '../assets/icons/uqtr.png',
@@ -54,7 +54,7 @@ export class ExperiencesComponent {
       date: '2021',
       description:
         'Travail très diversifié. Travaux agricoles (tracteur/ désherbage) et travaux de laboratoire comprenant la préparation des produits ainsi que leur emballage. ' +
-        'Un petit peu de travaux administratifs aussi (organisation).',
+        'Un petit peu de travaux administratifs aussi (organisation). ',
       list: ['Opérations agricoles', 'Préparation et embouteillages des produits du laboratoire'],
       iconTitle: 'Pransens',
       iconPath: '../assets/icons/pranasens.png',
